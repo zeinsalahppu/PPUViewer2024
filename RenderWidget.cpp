046a6c21872da2d03e4ae2b1dffcce3606b9c29e
zeinsalahppu <zeinsalah@gmail.com>
2025-11-17 22:51:15 +0200

    draw clock with houses

--- RenderWidget.cpp
+++ RenderWidget.cpp
@@ -12,6 +12,8 @@
 #include <glut.h>
 #include <QPainter>
 
+#include <iostream>
+
 RenderWidget::RenderWidget(QWidget* parent) : QOpenGLWidget(parent)
 {
 
@@ -41,7 +43,7 @@ void RenderWidget::initializeGL()
   glClearColor(1.0, 1.0, 1.0, 0.0);
 
   glMatrixMode(GL_PROJECTION);
-  gluOrtho2D(0.0, 200.0, 0.0, 200.0);
+  gluOrtho2D(0.0, 600.0, 0.0, 600.0);
   // glOrtho(-2.0, 2.0, -2.0, 2.0, -100, 100);
 
 }
@@ -54,20 +56,60 @@ void RenderWidget::paintGL()
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
 
-  drawHouse(1, 1, 0); // yellow
 
-  glPushMatrix();
-  glTranslatef(60.0, 30.0, 0.0);
-  drawHouse(1, 0, 1); // magenta
-  glPopMatrix();
+  glBegin(GL_LINES);
+ //   glColor3f(r, g, b);
+    glVertex2i(0, 300);
+    glVertex2i(600, 300);
+    glVertex2i(300, 0);
+    glVertex2i(300, 600);
+  glEnd();
 
-  glPushMatrix();
-  glTranslatef(10.0, 50.0, 0.0);
-  drawHouse(1, 0, 0);
-  glPopMatrix();
+  #define PI 3.14159 
+ 
+  float dt = 2 * PI / 12;
+  float r = 200;
 
-  glRotatef(30.0, 0.0, 0.0, 1.0);
-  drawHouse(0, 1, 0);
+  for (float t = 0; t < PI; t += dt)
+  {
+    float x = 300 + r*cos(t);
+    float y = 300 + r*sin(t);
+    std::cout << t << ":" << x << ", " << y << std::endl;
+   
+    glPushMatrix();
+    glTranslatef(x, y, 0.0);
+//    glTranslatef(75.0, 75.0, 0.0);
+    glRotatef(t*180/PI- 90, 0.0, 0.0, 1.0);
+//    glTranslatef(-75.0, -75.0, 0.0);
+
+    drawHouse(t/6, 0, 0);
+    glPopMatrix();
+  }
+
+  //for (float t = 0; t < 500; t += 150)
+  //{
+  //  std::cout << t << std::endl;
+
+  //  glPushMatrix();
+  //  glTranslatef(t, t, 0.0);
+  //  drawHouse(1, 0, 0);
+  //  glPopMatrix();
+  //}
+
+  //drawHouse(1, 1, 0); // yellow
+
+  //glPushMatrix();
+  //glTranslatef(60.0, 30.0, 0.0);
+  //drawHouse(1, 0, 1); // magenta
+  //glPopMatrix();
+
+  //glPushMatrix();
+  //glTranslatef(10.0, 50.0, 0.0);
+  //drawHouse(1, 0, 0);
+  //glPopMatrix();
+
+  //glRotatef(30.0, 0.0, 0.0, 1.0);
+  //drawHouse(0, 1, 0);
 
   //glTranslatef(75.0, 75.0, 0.0);
   //glRotatef(30.0, 0.0, 0.0, 1.0);
